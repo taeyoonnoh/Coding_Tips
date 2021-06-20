c5d337cc455f94ff9a57d6953644c1bc3e360f7d
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-06-20 15:11:03 +0900

    Create 2468번 : 안전영역.py

---
+++ [노태윤]/Good Problems/Baekjoon/2468번 : 안전영역.py
@@ -0,0 +1,48 @@
+import copy
+import sys
+sys.setrecursionlimit(1000000)
+
+N = int(input())
+graph = []
+min_val = int(1e9)
+max_val = 0
+for i in range(N) :
+    rows = list(map(int,sys.stdin.readline().split()))
+    check_min = min(rows)
+    check_max = max(rows)
+    if min_val > check_min : 
+        min_val = check_min
+    if max_val < check_max : 
+        max_val = check_max 
+    graph.append(rows)
+
+def dfs(x,y) : 
+    if x<0 or x>=N or y<0 or y>=N : 
+        return False
+    if copy_graph[x][y] == 1 : 
+        copy_graph[x][y] = 0 
+
+        dfs(x-1,y)
+        dfs(x+1,y)
+        dfs(x,y-1)
+        dfs(x,y+1)
+        return True
+    return False
+
+answer_list = []
+
+for i in range(min_val-1,max_val+1) : # min_val - 1 해준 이유는 다 같은 숫자로 구성된 graph가 있을 수도 있기 때문
+    copy_graph = copy.deepcopy(graph)
+    for k in range(N) : 
+        for j in range(N) : 
+            if copy_graph[k][j] <= i :
+                copy_graph[k][j] = 0
+            else : 
+                copy_graph[k][j] = 1
+    counts = 0
+    for k in range(N) : 
+        for j in range(N) : 
+            if dfs(k,j) == True : 
+                counts+=1
+    answer_list.append(counts)
+print(max(answer_list))
