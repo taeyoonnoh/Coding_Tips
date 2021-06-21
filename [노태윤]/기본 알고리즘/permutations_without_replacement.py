2e0c6fe7bbd71e3577d8c1fbd1a0cac97a8fe995
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-06-21 15:55:50 +0900

    Create permutations_without_replacement.py

---
+++ [노태윤]/기본 알고리즘/permutations_without_replacement.py
@@ -0,0 +1,10 @@
+N = int(input())
+answer = 0
+for i in range(1,N+1) : 
+	answer+= 3**i
+print(answer)
+
+# ==========================
+# 3 개중 1개씩 --> 6개중 2개씩 --> 9개중 3개씩
+# len(set(permutations,i)) 형태로 구할 수도 있지만 시간 초과뜸..
+# 결국에는 3**i 와 마찬가지다 
