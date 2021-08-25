eceb4d2b75f12e8b5218c06e330530c72fe14406
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-08-25 22:20:22 +0900

    Update solution.py

--- [노태윤]/Good Problems/Programmers/어려운 문제/Greedy/조이스틱/solution.py
+++ [노태윤]/Good Problems/Programmers/어려운 문제/Greedy/조이스틱/solution.py
@@ -1,5 +1,5 @@
 def solution(name) :
-    dist_list = [min(ord(i)-ord('A'),ord('Z')-ord(i)+1) for i in name]
+    dist_list = [min(ord(i)-ord('A'),ord('Z')-ord(i)+1) for i in name] # 한번에 바로 풀어버리기!
     idx,answer = 0,0
     
     while True : 
@@ -9,11 +9,11 @@ def solution(name) :
             return answer
         
         left,right = 1,1
-        while dist_list[idx-left] == 0 :
+        while dist_list[idx-left] == 0 : # 좌로 
             left+=1
-        while dist_list[idx+right] ==0:
+        while dist_list[idx+right] ==0: # 우로
             right+=1
 
-        answer += left if left<right else right
-        idx += -left if left<right else right
+        answer += left if left<right else right # 왼쪽으로 움직인게 더 적다면
+        idx += -left if left<right else right # idx 갱신!
     return dist_list
