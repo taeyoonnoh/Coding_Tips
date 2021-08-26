b774db30ea4016e26db71c52fa5938cd0fcc56cf
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-08-26 11:31:13 +0900

    Update Sorting.py

--- [노태윤]/Python Tips/Sorting.py
+++ [노태윤]/Python Tips/Sorting.py
@@ -11,3 +11,18 @@ new_files = [re.split('([0-9]+)',file) for file in files]
 sorted_list = sorted(new_files, key =lamda x : (x[0].lower(),int(x[1])))
 
 sorted_list = sorted(new_files, key =lamda x : (x[0].lower(),-int(x[1]))) # 이렇게 - 부호를 넣어주면 오름차순으로도 가능함
+
+# ============================================================================================================================
+
+# 그나마 효율적인 sorting 방법
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [0 for i in range(10001)] # 미리 배열 사이즈 정해놓기
+for _ in range(N) :
+    num = int(input())
+    arr[num]+=1 # 1씩 증가!
+for i in range(10001):
+    for j in range(arr[i]):
+        print(i)
