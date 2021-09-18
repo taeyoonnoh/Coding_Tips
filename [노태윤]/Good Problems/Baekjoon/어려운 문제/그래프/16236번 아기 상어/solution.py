f8566dd230b21936c06b083c9d074f4e442e507f
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2021-09-18 11:28:39 +0900

    Update solution.py

--- [노태윤]/Good Problems/Baekjoon/어려운 문제/그래프/16236번 아기 상어/solution.py
+++ [노태윤]/Good Problems/Baekjoon/어려운 문제/그래프/16236번 아기 상어/solution.py
@@ -1,3 +1,6 @@
+# 첫번째 시도
+# 실패
+
 from collections import deque
 import heapq
 
@@ -52,3 +55,56 @@ def bfs(x,y) :
     print(ans)
 
 bfs(shark_x,shark_y)
+
+# =======================================================================================================================
+
+# 2번째 시도
+from collections import deque
+import heapq
+
+N = int(input())
+graph = [list(map(int,input().split())) for _ in range(N)]
+
+drow = [0,1,0,-1]
+dcol = [1,0,-1,0]
+
+q = []
+
+for i in range(N) :
+    for j in range(N) : 
+        if graph[i][j] == 9 : 
+            heapq.heappush(q,(0,i,j))
+            graph[i][j] = 0
+
+def bfs() : 
+    ans,size,eat = 0,2,0
+    visited = [[False]*N for _ in range(N)]
+    while q : 
+        dist,x,y = heapq.heappop(q)
+        if 0 < graph[x][y] < size : 
+            eat+=1
+            graph[x][y] = 0 
+            if eat == size : 
+                size+=1
+                eat = 0
+            ans+=dist
+            dist = 0 
+            while q : # 만약 조건에 맞는 x,y 좌표에 도달한다면 q 에 있는 원소들 다 없애주고 visited 도 다시 초기화 해주는 작업이 중요!
+                q.pop()
+            visited = [[False]*N for _ in range(N)]
+        for i in range(4) : 
+            ddist,dx,dy = dist+1,x+drow[i],y+dcol[i]
+            if dx<0 or dx>=N or dy<0 or dy>= N : 
+                continue
+            if 0 < graph[dx][dy] and graph[dx][dy] > size or visited[dx][dy] : 
+                continue
+            visited[dx][dy] = True
+            heapq.heappush(q,(ddist,dx,dy))
+    print(ans)
+
+bfs()
+
+# ================================================================================================================================
+# visited 를 계속해서 초기화 시켜줘야 함
+# 좀만 더 생각하면 쉽게 풀 수 있는 문제라고 생각 함..ㅜㅜ
+# 그래프는 참 어렵다
