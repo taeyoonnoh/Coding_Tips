fc0ec1b040b32ce3570a0e7ba20b5a4de8d2ad0e
taeyoonnoh <74780115+taeyoonnoh@users.noreply.github.com>
2022-07-14 22:41:26 +0900

    Update characters.cpp

--- [노태윤]/C++/C++ Tips/characters.cpp
+++ [노태윤]/C++/C++ Tips/characters.cpp
@@ -21,3 +21,27 @@ int main() {
 }
 
 // cnt[a-'a'] 를 하게 되면 배열 크기를 줄일 수 있다!
+
+#include <iostream>
+
+using namespace std;
+
+int N, cnt[26];
+string s, ans;
+
+int main() {
+
+    cin >> N;
+    for (int i=0; i<N; i++) {
+        cin >> s;
+        cnt[s[0] - 'a']+=1;
+    }
+
+    for (int i=0; i<26; i++) if (cnt[i]>=5) ans += (i+'a');
+    if (ans.size()) cout << ans;
+    else cout << "PREDAJA";
+
+    return 0;
+}
+
+// i + 'a' 하면 자동으로 type casting 되어서 character 형태로 나온다
